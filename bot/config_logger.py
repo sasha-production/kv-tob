b905d9ba1cb52da1f4711fa03bd568cdd9d55d70
sasha-production <sasha.ignatov93@gmail.com>
2025-07-21 01:22:31 +0500

    lg files rotation by size

--- bot/config_logger.py
+++ bot/config_logger.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-
+from logging.handlers import RotatingFileHandler
 from bot.bot_data import BASE_DIR
 
 
@@ -13,6 +13,13 @@ def setup_logger(name):
     formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s')
 
     file_handler = logging.FileHandler(filename=BASE_DIR / "data" / "bot.logs")
+   # RotatingFileHandler - ротация по размеру файла
+    file_handler = RotatingFileHandler(
+        filename=BASE_DIR / "data" / "bot.logs",
+        maxBytes=1024*1024,  # 1 MB
+        backupCount=1,  # сохранять 1 резервных копий
+        encoding='utf-8'
+    ) 
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
